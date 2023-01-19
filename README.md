aff41448012c539e3b07417c4e6f5ff004dad206
napia-san <bn18250@shibaura-it.ac.jp>
2023-01-19 05:18:26 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Working Time Recorder
+Google Calendar APIを使って作業時間の記録を行うWebアプリ
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/ress.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Working time recorder</title>
+    </head>
+    
+</html>
