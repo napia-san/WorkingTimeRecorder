f37af4fe279ac15753ca178b4b523206f3649705
napia-san <bn18250@shibaura-it.ac.jp>
2023-01-19 05:36:32 +0000

    モックアップ

--- index.php
+++ index.php
@@ -6,5 +6,23 @@
         <link rel="stylesheet" href="css/style.css">
         <title>Working time recorder</title>
     </head>
-    
+    <header>
+		<div class="headerLeft">
+			<h1>Working time recorder</h1>
+		</div>
+    </header>
+    <body>
+        <form class="inputs" method="post">
+            <label class="input_label">ID: </label><input class="text_box" name="id"><br>
+            <input class="btn" type="submit" value="開始" name="start">
+            <input class="btn" type="submit" value="終了"name="finish">
+        </form>
+        <div class="others">
+            <button class="btn btn_register" onclick="location.href='registration.html'">利用登録</button>
+        </div>
+    </body>
 </html>
+
+<?php
+
+?>
---
+++ registration.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/ress.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Registration | Working time recorder</title>
+    </head>
+    <header>
+		<div class="headerLeft">
+			<h1>Working time recorder - Registration</h1>
+		</div>
+    </header>
+    <body>
+        <form class="inputs" method="post">
+            <label class="input_label">ID: </label><input class="text_box" name="user_id"><br>
+            <label class="input_label">ユーザー名: </label><input class="text_box" name="user_name"><br>
+            <label class="input_label">GoogleカレンダーID: </label><input class="text_box" name="cal_id"><br>
+            <input class="btn" type="submit" value="登録" name="register">
+        </form>
+    </body>
+</html>
+
+<?php
+
+?>
